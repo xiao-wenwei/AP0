7cb646aec270c0a22384f7b3508b6777bde535a0
GiorgioPeng <84867579@qq.com>
2020-03-18 10:23:53 +0000

    Deploying to gh-pages from master 1d9c56bdfff097e7e60b03e21d4f279534463dc0

---
+++ precache-manifest.4dbc15464a35ddc97aea18c3968329b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf92c44e1a62ecaf0f1fbbf46632b65d",
+    "url": "/AP-coursework-front-end/index.html"
+  },
+  {
+    "revision": "01204929b832255a6e8c",
+    "url": "/AP-coursework-front-end/static/css/main.4ea85f98.chunk.css"
+  },
+  {
+    "revision": "24299c7c7c6734406a9a",
+    "url": "/AP-coursework-front-end/static/js/2.3022bb85.chunk.js"
+  },
+  {
+    "revision": "4695e1fd83fa1b40931b6b4ad59269fe",
+    "url": "/AP-coursework-front-end/static/js/2.3022bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01204929b832255a6e8c",
+    "url": "/AP-coursework-front-end/static/js/main.83954de3.chunk.js"
+  },
+  {
+    "revision": "8d459d9c0e9d7c0e6149",
+    "url": "/AP-coursework-front-end/static/js/runtime-main.44fcde36.js"
+  }
+]);
